b790902a0b93faa9940e1fc97c087e7bfd015547
Samuel Weese <weesesr@mail.uc.edu>
2023-02-20 22:50:26 -0500

    Base Minesweeper implemented

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "minesweeper.h"
+
+int main()
+{
+    const static int windowSizeHeight = 720;
+    const static int windowSizeLength = 720;
+    sf::RenderWindow window(sf::VideoMode(windowSizeLength, windowSizeHeight), "SFML window");
+    minesweeper aGame(&window, 10, 10);
+    aGame.createRBoard();
+    aGame.printAnswer();
+    window.setFramerateLimit(60);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            case sf::Event::Resized:
+                break;
+            case sf::Event::MouseButtonPressed:
+                aGame.mouse(event);
+                break;
+            case sf::Event::KeyPressed:
+                aGame.keys(event);
+                break;
+            }
+
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+        }
+
+        window.clear();
+
+        aGame.draw();
+        window.display();
+    }
+
+    return 0;
+}
---
+++ minesweeper.h
@@ -0,0 +1,58 @@
+#ifndef MINESWEEPER_H
+#define MINESWEEPER_H
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <vector>
+
+#define bomb 9
+
+struct coordPair
+{
+    u_int x;
+    u_int y;
+};
+
+
+
+class minesweeper
+{
+    std::vector<std::vector<u_int>>boardState;
+    std::vector<std::vector<bool>> boardVisible;
+    u_int size;
+    u_int bombs;
+    std::vector<coordPair> flags;
+    bool playing;
+
+    u_int padding;
+
+    sf::RenderWindow *window;
+    sf::Texture spriteSheet;
+    std::vector<std::vector<sf::Sprite>> spriteVector;
+public:
+    minesweeper(sf::RenderWindow *aWindow, u_int size = 10, u_int bombs = 10);
+    ~minesweeper();
+    void createRBoard(int seed = 0);
+    void seedRBoard(int seed = 0);
+    void numberBoard();
+    void solve();
+
+    void draw();
+    void print();
+    void printAnswer();
+
+    void mouse(sf::Event);
+    void keys(sf::Event );
+
+    void cleanBoardVisuals();
+    void resetBoardState();
+
+    void groupShow(u_int x, u_int y);
+    void activateTile(u_int x, u_int y);
+    void flag(u_int x, u_int y);
+
+
+    void endGame(bool winner);
+
+};
+
+#endif // MINESWEEPER_H
